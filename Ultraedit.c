44526f1e6d7bb75fd334fd324fe3934078c9badd
karsn <karsn@@126.com>
2020-03-12 22:00:23 +0800

    Maintain: Create

---
+++ Ultraedit.c
@@ -0,0 +1,32 @@
+/*******************************************************************************
+Copyright(R) 2018-2028, EDoo Tech. Co., Ltd
+File Name: [FILE_NAME][FILE_EXTENSION]
+Author: Karsn           Date: [DATE_USER_SYS]yyyy-MM-dd[DATE_USER_SYS_END]             Version: 1.0
+Discription:
+
+Function:
+
+History:
+1. Version: 1.0        Date: [DATE_USER_SYS]yyyy-MM-dd[DATE_USER_SYS_END]         Author: Karsn
+    Modification:
+      Creation.
+*******************************************************************************/
+
+/*******************************************************************************
+Function: 
+Description:
+Calls:
+Called By:
+Table Accessed:
+Table Updated:
+Input:
+Output:
+Return:
+Others:
+*******************************************************************************/
+
+/****************************** Private typedef *******************************/
+/****************************** Private define ********************************/
+/****************************** Private macro *********************************/
+/***************************** Private variables ******************************/
+/************************ Private function prototypes *************************/
---
+++ merge_bl.bat
@@ -0,0 +1,2 @@
+mergehex -m IAP.hex .\01MotorDrv\Exe\01MotorDrv.hex -o .\01MotorDrv\Exe\01MotorDrv.hex
+exit
